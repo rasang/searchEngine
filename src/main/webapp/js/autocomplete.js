8db463ead6735b8991d606beedef647fd1e7d8c4
rasang <1285847284@qq.com>
2020-01-09 19:09:13 +0800

    修改了前端js请求联想词的方法，解决了用户输入阻塞的问题

--- src/main/webapp/js/autocomplete.js
+++ src/main/webapp/js/autocomplete.js
@@ -1,39 +1,30 @@
 function enterToSearch() {
-  var input = document.getElementsByClassName("search-button");
-  input.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode == 13) {
-      document.getElementsByClassName("search-button").click();
-    }
-  });
-}
-
-function getData(url) {
-  var jsonData = "";
-  $.ajax({
-    type: "get",
-    url: url,
-    datatype: "json",
-    async: false,
-    error: function() {
-      console.error("Load recommand data failed!");
-    },
-    success: function(data) {
-      jsonData = data;
-    }
-  });
-  return jsonData;
-}
-
-$(function () {
-	  $(".search-input").keyup(
-	    function(event){
-	      setTimeout(function(){
-	        recommandData=JSON.parse(getData("suggest.jsp?term="+document.getElementById("input").value));
-	        $(".search-input").autocomplete({
-	          source: recommandData
-	        });
-	      },2000)
-	    }
-	  )
+	var input = document.getElementsByClassName("search-button");
+	input.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode == 13) {
+			document.getElementsByClassName("search-button").click();
+		}
 	});
+}
+
+$(function() {
+	$(".search-input").keyup(function(event) {
+		var jsonData = "";
+		$.ajax({
+			type : "get",
+			url : "suggest.jsp?term=" + document.getElementById("input").value,
+			datatype : "json",
+			async : true,
+			error : function() {
+				console.error("Load recommand data failed!");
+			},
+			success : function(data) {
+				data = JSON.parse(data);
+				$(".search-input").autocomplete({
+					source : data
+				});
+			}
+		});
+	})
+});
